469233f41cc98cf32f800f178fcdd9ee04514083
Victor Belosevich Mugerli <victor.belosevich94@gmail.com>
2016-11-09 10:26:35 -0300

    Modifico la sintaxis de aab.c

--- abb.c
+++ abb.c
@@ -24,11 +24,11 @@ typedef struct abb_iter{
 
 
 /******************************************************************
- *										  FUNCIONES AUXILIARES                      *
+ *		                  FUNCIONES AUXILIARES                      *
  ******************************************************************/
 
 nodo_abb_t* nodo_abb_crear(const char* clave, void* dato){
-	nodo_abb_t* nodo = malloc(sizeof(nodo_abb_t));
+  nodo_abb_t* nodo = malloc(sizeof(nodo_abb_t));
   if (!nodo) return NULL;
   nodo->dato = dato;
   nodo->clave = clave;//habría que hacer un strdup(copiar la clave)
@@ -38,7 +38,7 @@ nodo_abb_t* nodo_abb_crear(const char* clave, void* dato){
 }
 
 nodo_abb_t* buscar_nodo(nodo_abb_t* nodo, abb_comparar_clave_t cmp, const char* clave, nodo_abb_t** padre){
- 	if (!nodo) return NULL;
+   if (!nodo) return NULL;
    int resultado = cmp(nodo->clave, clave);
    if (resultado == 0) return nodo;
    *padre = nodo;
@@ -59,8 +59,7 @@ bool _abb_guardar(nodo_abb_t* padre, const char* clave, void* dato, int lado){
 }
 
 bool _abb_reemplazar_nodo(abb_t* arbol, const char* clave, void* dato){
-     // Caso clave preexistente
-     //nodo_abb* raiz = arbol->raiz;
+  // Caso clave preexistente
   nodo_abb_t* aux = buscar_nodo(arbol->raiz, arbol->cmp, clave, &(arbol->raiz));
   if(arbol->destructor) arbol->destructor(aux->dato);
   aux->dato = dato;
@@ -69,8 +68,8 @@ bool _abb_reemplazar_nodo(abb_t* arbol, const char* clave, void* dato){
 
 bool _abb_guardar_rec(abb_t* arbol, nodo_abb_t* actual, const char* clave, void* dato){
      // Primitiva auxiliar recursiva
-		 printf("%s\n", actual->clave);
-		 int res = arbol->cmp(actual->clave, clave);
+		 
+     int res = arbol->cmp(actual->clave, clave);
      if ((!actual->der && res == -1) || (!actual->izq && res == 1))
          return _abb_guardar(actual, clave, dato, res);
      if (res == -1) return _abb_guardar_rec(arbol, actual->der, clave, dato);
@@ -118,22 +117,23 @@ void *_abb_borrar(nodo_abb_t* nodo, nodo_abb_t* padre, nodo_abb_t** raiz){
 
 	nodo_abb_t* raiz_aux = *raiz;
 	if(nodo == padre) cambiar_raiz(&raiz_aux, cant_hijos);
+	
 	nodo_abb_t* padre_aux;
 	nodo_abb_t* nodo_aux;
 	switch(cant_hijos){
  	  case 0://Borrar sin hijos.
-    			  if (padre->der == nodo) padre->der = NULL;
+    	      if (padre->der == nodo) padre->der = NULL;
             else padre->izq = NULL;
-						dato = _borrar_hoja(nodo);
+	          dato = _borrar_hoja(nodo);
             break;
 
-		case 1://Borrar con 1 hijo.
- 		        if (nodo->der) padre->der = nodo->der;
+	  case 1://Borrar con 1 hijo.
+ 		  			if (nodo->der) padre->der = nodo->der;
             else padre->izq = nodo->izq;
             dato = _borrar_hoja(nodo);
             break;
 
-		default: //Borrar con 2 hijos.
+	  default: //Borrar con 2 hijos.
             padre_aux = nodo;
             nodo_aux = _buscar_max_izq(nodo->izq, &padre_aux);
             const char* clave_aux = nodo_aux->clave;
@@ -141,14 +141,14 @@ void *_abb_borrar(nodo_abb_t* nodo, nodo_abb_t* padre, nodo_abb_t** raiz){
             dato = nodo->dato;
  		        nodo->clave = clave_aux;
  		        nodo->dato = dato_aux;
-  }
+	}
 
 	*raiz = raiz_aux;
   return dato;
 }
 
 void _abb_destruir(nodo_abb_t* nodo, abb_destruir_dato_t destructor){
-	// Primitiva recursiva auxiliar
+  // Primitiva recursiva auxiliar
   if (!nodo) return;
   _abb_destruir(nodo->izq, destructor);
   _abb_destruir(nodo->der, destructor);
@@ -157,7 +157,7 @@ void _abb_destruir(nodo_abb_t* nodo, abb_destruir_dato_t destructor){
  }
 
 /******************************************************************
- *								        PRIMITIVAS ABB                          *
+ *			                  PRIMITIVAS ABB                          *
  ******************************************************************/
 
 abb_t* abb_crear(abb_comparar_clave_t cmp, abb_destruir_dato_t destruir_dato){
@@ -173,8 +173,7 @@ abb_t* abb_crear(abb_comparar_clave_t cmp, abb_destruir_dato_t destruir_dato){
 bool abb_guardar(abb_t *arbol, const char *clave, void *dato){
     if (!arbol) return false;
     nodo_abb_t* raiz = arbol->raiz;
-		printf("la clave al entrar a guardar %s\n", clave);
-		if (!raiz) { //arbol vacio
+    if (!raiz) { //arbol vacio
         raiz = nodo_abb_crear(clave, dato);
         if (!raiz) return false;
         arbol->raiz = raiz;
@@ -192,7 +191,6 @@ void *abb_borrar(abb_t *arbol, const char *clave){
     nodo_abb_t* nodo = buscar_nodo(arbol->raiz, arbol->cmp, clave, &padre);
     if(!nodo) return NULL;
     void *dato = _abb_borrar(nodo, padre, &(arbol->raiz));
-		printf("La Raiz es : %s\n", arbol->raiz->clave);
     arbol->cantidad --;
     return dato;
 }
@@ -206,7 +204,6 @@ void *abb_obtener(const abb_t *arbol, const char *clave){
 
 bool abb_pertenece(const abb_t *arbol, const char *clave){
     nodo_abb_t* raiz = arbol->raiz;
-		printf("Raiz en pertenece %s\n", raiz->clave);
     if (!buscar_nodo(arbol->raiz, arbol->cmp, clave, &raiz))
         return false;
     return true;
@@ -222,7 +219,7 @@ void abb_destruir(abb_t *arbol){
 }
 
 /******************************************************************
- *											  ITERADOR ABB                            *
+ *			                  ITERADOR ABB                            *
  ******************************************************************/
 
 void abb_iterar_izq(pila_t *pila, nodo_abb_t *nodo_abb){
@@ -249,14 +246,14 @@ bool abb_iter_in_al_final(const abb_iter_t *iter){
 const char *abb_iter_in_ver_actual(const abb_iter_t *iter){
 	if(abb_iter_in_al_final(iter))
 		return NULL;
-    nodo_abb_t* tope = pila_ver_tope(iter->pila);
+    	nodo_abb_t* tope = pila_ver_tope(iter->pila);
 	return tope->clave;
 }
 
 bool abb_iter_in_avanzar(abb_iter_t *iter){
 	if(abb_iter_in_al_final(iter))
 		return false;
-    nodo_abb_t* tope = pila_desapilar(iter->pila);
+    	nodo_abb_t* tope = pila_desapilar(iter->pila);
 	abb_iterar_izq(iter->pila, tope->der);
 	return true;
 }
